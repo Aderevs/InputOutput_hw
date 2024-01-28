2cb9821cb1d1f3ba9ba5984ddf5085c4cad62fee
Yura <aderevs@gmail.com>
2024-01-28 20:05:51 +0200

    task 2 working version with possible changes

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace Task1
+﻿namespace Task2
 {
     internal class Program
     {
+
+        public static void WriteToFile(string fileName)
+        {
+
+            var file = new FileInfo(fileName);
+            StreamWriter streamWriter = file.CreateText();
+            for (int i = 0; i < 10; i++)
+            {
+                streamWriter.WriteLine($"Random data: {i}");
+            }
+            streamWriter.Close();
+        }
+
+        public static void ReadFile(string fileName)
+        {
+            var file = new FileInfo(fileName);
+            if(file.Exists)
+            {
+                StreamReader streamReader = file.OpenText();
+                while(streamReader.Peek() != -1)
+                {
+                    string line = streamReader.ReadLine();
+                    Console.WriteLine(line);
+                }
+                streamReader.Close();
+            }
+            else
+            {
+                Console.WriteLine("file {0} does not exist", fileName);
+            }
+        }
+        
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            WriteToFile("randomData.txt");
+            ReadFile("randomData.txt");
         }
     }
 }
