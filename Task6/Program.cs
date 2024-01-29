b2de903f4afce4346332de995a9ef5a35ae11a54
Yura <aderevs@gmail.com>
2024-01-29 18:53:27 +0200

    task 6 working version

---
+++ Task6/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Task6
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string[] drives = Directory.GetLogicalDrives();
+            Console.WriteLine("all available disks:");
+            foreach (var drive in drives)
+            {
+                Console.Write(drive);
+            }
+            Console.WriteLine();
+            Console.WriteLine("enter name of disk you want to create folders:");
+            string disk = Console.ReadLine();
+            disk += ":\\";
+            for(int i = 0; i < 100; i++)
+            {
+                Directory.CreateDirectory(disk + $"Folder_{i}");
+            }
+            Console.WriteLine("folders was created");
+            for (int i = 0; i < 100; i++)
+            {
+                Directory.Delete(disk + $"Folder_{i}");
+            }
+            Console.WriteLine("folders was deleted");
+        }
+    }
+}
